ba760118dee6168557a1616246da7837d2786690
ANDRE Simon <simon@emencia.com>
2023-04-25 21:58:07 +0200

    Migrate test method from NinjaTestCase to TestNinjaTestCase + add NinjaTestCase helper variable to link api

--- tests/test_testcase.py
+++ tests/test_testcase.py
@@ -2,4 +2,22 @@ from .testcase import NinjaTestCase
 
 
 class TestNinjaTestCase(NinjaTestCase):
-    pass
+    def test_get_public(self):
+        res = self.client.get("/")
+        assert res.status_code == 200
+
+    def test_get_anonymous(self):
+        res = self.client.get(f"{self.root_path}{self.docs_url[1:]}")
+        assert res.status_code == 302
+
+    def test_get_authentificated(self):
+        self.client.force_login(self.user)
+        res_redirect = self.client.get(f"{self.root_path}{self.docs_url[1:]}")
+        assert res_redirect.status_code == 302
+        res = self.client.get(f"{self.root_path}login")
+        assert res.status_code == 200
+
+    def test_get_admin_authentificated(self):
+        self.client.force_login(self.admin_user)
+        res = self.client.get(f"{self.root_path}{self.docs_url[1:]}")
+        assert res.status_code == 200
--- tests/testcase.py
+++ tests/testcase.py
@@ -5,34 +5,19 @@ from django.contrib.auth import get_user_model
 
 class NinjaTestCase(TestCase):
     def setUp(self):
+        # Create test user
         self.user = get_user_model().objects.create_user(
             username="user", password="user"
         )
         self.admin_user = get_user_model().objects.create_superuser(
             username="admin", password="admin"
         )
+        # Create test client
         self.user_client = self.client_class()
         self.user_client.force_login(self.user)
-
         self.admin_client = self.client_class()
         self.admin_client.force_login(self.admin_user)
-
-    def test_get_public(self):
-        res = self.client.get("/")
-        assert res.status_code == 200
-
-    def test_get_anonymouss(self):
-        res = self.client.get(f"{api.root_path}{api.docs_url[1:]}")
-        assert res.status_code == 302
-
-    def test_get_authentificated(self):
-        self.client.force_login(self.user)
-        res_redirect = self.client.get(f"{api.root_path}{api.docs_url[1:]}")
-        assert res_redirect.status_code == 302
-        res = self.client.get(f"{api.root_path}login")
-        assert res.status_code == 200
-
-    def test_get_admin_authentificated(self):
-        self.client.force_login(self.admin_user)
-        res = self.client.get(f"{api.root_path}{api.docs_url[1:]}")
-        assert res.status_code == 200
+        # Link api
+        self.api = api
+        self.root_path = self.api.root_path
+        self.docs_url = self.api.docs_url
