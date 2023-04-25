3ee939eae2752b12f8b75d735436ddff76072d27
ANDRE Simon <simon@emencia.com>
2023-04-25 22:17:58 +0200

    Some unittest docstrings

--- tests/testcase.py
+++ tests/testcase.py
@@ -5,21 +5,20 @@ from django.contrib.auth import get_user_model
 
 class NinjaTestCase(TestCase):
     def create_client_helper(self):
-        # Create test user
+        """Create test user and log them to dedicated client."""
         self.user = get_user_model().objects.create_user(
             username="user", password="user"
         )
         self.admin_user = get_user_model().objects.create_superuser(
             username="admin", password="admin"
         )
-        # Create test client
         self.user_client = self.client_class()
         self.user_client.force_login(self.user)
         self.admin_client = self.client_class()
         self.admin_client.force_login(self.admin_user)
 
     def link_api_helper(self):
-        # Link api
+        """Can override this method to link another project api to the test case."""
         self.api = api
         self.root_path = self.api.root_path
         self.docs_url = self.api.docs_url
